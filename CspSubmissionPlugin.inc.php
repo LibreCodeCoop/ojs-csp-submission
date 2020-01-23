8039f2abd4d736aef5e36f55887828f338eb159a
LiviaGouvea <gouvea.lc@gmail.com>
2020-01-23 00:01:27 -0300

    Refactoring task #23. Now using template override.

--- CspSubmissionPlugin.inc.php
+++ CspSubmissionPlugin.inc.php
@@ -49,13 +49,10 @@ class CspSubmissionPlugin extends GenericPlugin {
 
 	function additionalMetadataStep1($hookName, $args) {
 		if ($args[1] == 'submission/form/step1.tpl') {
-			$templateMgr =& $args[0];			
-			$templateMgr->assign('additionalFormContent1', $templateMgr->fetch($this->getTemplateResource('IndicacaoConsultor.tpl')));
-
-		} elseif ($args[1] == 'submission/form/step2.tpl') {
 			$templateMgr =& $args[0];
-			$templateMgr->assign('additionalFormContent1', $templateMgr->fetch($this->getTemplateResource('IndicacaoConsultor.tpl')));
-
+			$args[4] = $templateMgr->fetch($this->getTemplateResource('step1.tpl'));
+			
+			return true;
 		} elseif ($args[1] == 'controllers/wizard/fileUpload/form/submissionArtworkFileMetadataForm.tpl') {
 			$templateMgr =& $args[0];
 			$args[4] = $templateMgr->fetch($this->getTemplateResource('submissionArtworkFileMetadataForm.tpl'));
--- templates/IndicacaoConsultor.tpl
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-
-$(function () {
-	$("<div>Considerando que parte da demora na avaliação do artigo se deve à busca de consultores, agradecemos a indicação de ao menos dois nomes (obrigatório).</div>").insertAfter("label[for^='commentsToEditor']");
-
-});
-
-</script>
