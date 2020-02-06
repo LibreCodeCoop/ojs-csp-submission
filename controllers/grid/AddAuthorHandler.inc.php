6793d73e071c2e09307e4819ab7ffb0168142ac7
Vitor Mattos <vitor@php.rio>
2020-02-06 20:58:44 -0300

    Passagem de dados do autor do SAGAS para o OJS

--- controllers/grid/AddAuthorHandler.inc.php
+++ controllers/grid/AddAuthorHandler.inc.php
@@ -12,24 +12,58 @@ class AddAuthorHandler extends GridHandler {
     public function searchAuthor($args, $request)
     {
         AppLocale::requireComponents(LOCALE_COMPONENT_PKP_SUBMISSION, LOCALE_COMPONENT_PKP_USER);
-        $authorId = (int) $request->getUserVar('authorId');
-        $submissionDAO = Application::getSubmissionDAO();
-        $submission = $submissionDAO->getById($request->getUserVar('submissionId'));
 
-        $authorDao = DAORegistry::getDAO('AuthorDAO');
-        $author = $authorDao->getById($authorId, $submission->getId());
-
-        // Form handling
         import('lib.pkp.controllers.grid.users.author.form.AuthorForm');
-        $authorForm = new AuthorForm($submission, $author, 'submissionId');
-        $authorForm->setTemplate($this->plugin->getTemplateResource('authorFormAdd.tpl'));
+        $form = new Form($this->plugin->getTemplateResource('authorFormAdd.tpl'));
 
-        $userService = ServicesContainer::instance()->get('user');
-        $user = $userService->getUser($request->getUserVar('userId'));
-        $authorForm->_data = $user->_data;
-        $authorForm->setData('includeInBrowse', true);
-        $authorForm->setData('csrfToken', $request->getSession()->getCSRFToken());
+        $countryDao = DAORegistry::getDAO('CountryDAO');
+        $countries = $countryDao->getCountries();
+        $form->setData('countries', $countries);
 
-        return new JSONMessage(true, $authorForm->fetch($request));
+        if ($args['type'] == 'csp') {
+            $userDao = DAORegistry::getDAO('UserDAO');
+            $userCsp = $userDao->retrieve(
+                <<<QUERY
+                SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(p.nome, ' ', 1), ' ', -1) as given_name,
+                       TRIM( SUBSTR(p.nome, LOCATE(' ', p.nome)) ) family_name,
+                       pais, email, orcid
+                  FROM csp.Pessoa p
+                 WHERE p.idPessoa = ?
+                QUERY,
+                [$args['userId']]
+            )->GetRowAssoc(0);
+            $locale = AppLocale::getLocale();
+            $form->setData('givenName', [$locale => $userCsp['given_name']]);
+            $form->setData('familyName', [$locale => $userCsp['family_name']]);
+            $form->setData('email', $userCsp['email']);
+            $form->setData('orcid', $userCsp['orcid']);
+            if (!empty($userCsp['pais'])) {
+                $abbreviation = array_search($userCsp['pais'], $countries);
+                if ($abbreviation) {
+                    $form->setData('country', $abbreviation);
+                }
+            }
+        } else {
+            $userService = ServicesContainer::instance()->get('user');
+            $user = $userService->getUser($request->getUserVar('userId'));
+            // $form->_data = $user->_data;
+            $form->setData('givenName', $user->getData('givenName'));
+            $form->setData('familyName',$user->getData('familyName'));
+            $form->setData('email', $user->getData('email'));
+            $form->setData('country', $user->getData('country'));
+            $form->setData('orcid', $user->getData('orcid'));
+        }
+
+        $router = $request->getRouter();
+        $context = $router->getContext($request);
+        $userGroupDao = DAORegistry::getDAO('UserGroupDAO');
+        $authorUserGroups = $userGroupDao->getByRoleId($context->getId(), ROLE_ID_AUTHOR);
+        $form->setData('authorUserGroups', $authorUserGroups);
+
+        $form->setData('submissionId', $args['submissionId']);
+        $form->setData('includeInBrowse', true);
+        $form->setData('csrfToken', $request->getSession()->getCSRFToken());
+
+        return new JSONMessage(true, $form->fetch($request));
     }
 }
