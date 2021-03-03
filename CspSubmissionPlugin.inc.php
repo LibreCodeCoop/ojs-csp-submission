b64a356271c1d12813828998919eaf730025de56
LiviaGouvea <gouvea.lc@gmail.com>
2021-03-03 17:02:20 -0300

    Correct to display co-author data when searching the ojs database

--- CspSubmissionPlugin.inc.php
+++ CspSubmissionPlugin.inc.php
@@ -1830,11 +1830,21 @@ class CspSubmissionPlugin extends GenericPlugin {
 
 	public function authorform_initdata($hookName, $args)
 	{
-
+		$locale = AppLocale::getLocale();
 		$request = \Application::get()->getRequest();
 		$type = $request->getUserVar('type');
 		$form = $args[0];
-		if ($type == 'csp') {
+		if ($type == 'ojs') {
+			$userId = $request->getUserVar('userId');
+			$userDao = DAORegistry::getDAO('UserDAO'); /* @var $userDao UserDAO */
+			$user = $userDao->getById($userId);
+			$form->setData('givenName', [$locale => $user->getlocalizedGivenName()]);
+			$form->setData('familyName', [$locale => $user->getlocalizedfamilyName()]);
+			$form->setData('affiliation', [$locale =>  $user->getlocalizedAffiliation()]);
+			$form->setData('email',  $user->getEmail());
+			$form->setData('orcid',  $user->getOrcid());
+			$form->setData('country',  $user->getCountry());
+		}elseif ($type == 'csp') {
 			$userDao = DAORegistry::getDAO('UserDAO');
 			$userCsp = $userDao->retrieve(
 				<<<QUERY
@@ -1847,13 +1857,12 @@ class CspSubmissionPlugin extends GenericPlugin {
 				QUERY,
 				[(int) $request->getUserVar('userId')]
 			)->GetRowAssoc(0);
-			$locale = AppLocale::getLocale();
 			$form->setData('givenName', [$locale => $userCsp['given_name']]);
 			$form->setData('familyName', [$locale => $userCsp['family_name']]);
 			$form->setData('affiliation', [$locale => $userCsp['affiliation']]);
 			$form->setData('email', $userCsp['email']);
 			$form->setData('orcid', $userCsp['orcid']);
-		}elseif($form->_author != null){
+		}elseif($form->getAuthor() != null){
 			$form->setTemplate($this->getTemplateResource('authorFormAdd.tpl'));
 			$author = $form->_author;
 			$form->setData('authorContribution', $author->getData('authorContribution'));
