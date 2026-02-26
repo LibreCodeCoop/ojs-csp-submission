541c52349bafd8864542d1289abfbf64f57df2ac
Lívia Gouvêa <livia@mail.mayfirst.org>
2026-02-26 14:04:48 -0300

    Remove alterações em Vue.js que não estavam funcionando corretamente

--- js/load.js
+++
@@ -1,9 +0,0 @@
-import PkpLoad from '../../../../lib/pkp/js/load.js';
-
-import SubmissionWizardPage from '@csp/components/Conteiner/SubmissionWizardPage.vue';
-
-window.pkp = Object.assign(PkpLoad, {
-	controllers: {
-		SubmissionWizardPage
-	},
-});
